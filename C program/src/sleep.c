a7dd4e4867deb1e72eef17d088349e807c4c9950
SebastianSoerensen <Sebastiansoerensen97@outlook.dk>
2026-01-13 16:49:15 +0100

    .

--- C program/src/sleep.c
+++ C program/src/sleep.c
@@ -5,5 +5,6 @@ void go_to_sleep(void){
     sleep_enable();
     sleep_cpu();
     // The CPU is sleeping at this point, only to be waken up by interrupt. 
+    // Once CPU is waken by interrupt, sleep_disable() is called, and system is running again. 
     sleep_disable();
 }
